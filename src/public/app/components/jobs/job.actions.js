f1c5c61fbb8b638f95ef0d16f08d1ae5bb35bb93
Luciano Neucamp <lucianoneucamp@gmail.com>
2016-10-14 13:56:07 -0300

    fix api url

--- src/public/app/components/jobs/job.actions.js
+++ src/public/app/components/jobs/job.actions.js
@@ -2,13 +2,10 @@ import axios from 'axios'
 import JobConstants from './job.constants'
 import {dispatcher} from '../../dispatchers/AppDispatcher.js'
 
-console.debug("aaatip")
-const url = API_URL
-
 export default {
     searchJob(q){
         console.debug("JobAction.searchJob",q)
-        axios.get(`${url}/api/jobs`,{params:{q}}).then(function(response){
+        axios.get(`/api/jobs`,{params:{q}}).then(function(response){
             const {data} = response
             dispatcher({actionType:JobConstants.SEARCH_JOB,data})
         },function (err) {
